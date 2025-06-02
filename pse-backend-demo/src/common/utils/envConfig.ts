da791590f841404ac86f8b087fe814ef291b2e54
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2025-06-02 11:06:12 +0200

    remove default empty

--- pse-backend-demo/src/common/utils/envConfig.ts
+++ pse-backend-demo/src/common/utils/envConfig.ts
@@ -7,9 +7,9 @@ const envSchema = z.object({
   NODE_ENV: z.enum(["development", "production", "test"]).default("production"),
   HOST: z.string().min(1).default("localhost"),
   PORT: z.coerce.number().int().positive().default(8080),
-  CLIENT_CERT: z.string().min(1).default(""),
-  CLIENT_KEY: z.string().min(1).default(""),
-  GNOSIS_PSE_PRIVATE_API_BASE_URL: z.string().url().default(""),
+  CLIENT_CERT: z.string().min(1),
+  CLIENT_KEY: z.string().min(1),
+  GNOSIS_PSE_PRIVATE_API_BASE_URL: z.string().url().min(1),
 });
 
 const parsedEnv = envSchema.safeParse(process.env);
