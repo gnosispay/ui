680c0869d69e9d58b06afb45d241e1e87a7bfa16
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2025-06-11 15:25:18 +0200

    support GPB and USD

--- src/components/balances.tsx
+++ src/components/balances.tsx
@@ -2,26 +2,43 @@ import { useUser } from "@/context/UserContext";
 import { useMemo } from "react";
 import { Clock } from "lucide-react";
 import { Skeleton } from "./ui/skeleton";
+import { currencies, type CurrencyInfo } from "../constants";
 
-const formatCurrency = (value: string | undefined): string | undefined => {
-  if (!value) {
+const formatCurrency = (value: string | undefined, currencyInfo: CurrencyInfo | undefined): string | undefined => {
+  if (!value || !currencyInfo) {
     return undefined;
   }
   try {
     const bigIntValue = BigInt(value);
-    const valueInUnits = Number(bigIntValue) / 10 ** 18; // Changed to 10^18 for correct Euro conversion
-    return new Intl.NumberFormat("en-US", { style: "currency", currency: "EUR" }).format(valueInUnits);
+    const valueInUnits = Number(bigIntValue) / 10 ** currencyInfo.decimals;
+    return new Intl.NumberFormat("en-US", { style: "currency", currency: currencyInfo.fiatSymbol }).format(
+      valueInUnits,
+    );
   } catch (e) {
     console.error("Error formatting currency:", e);
-    return "€NaN";
+    return `"${currencyInfo.symbol}NaN`;
   }
 };
 
 export const Balances = () => {
-  const { balances } = useUser();
+  const { balances, safeConfig } = useUser();
+  const currencyInfo = useMemo(() => {
+    if (!safeConfig?.fiatSymbol) {
+      return
+    };
+    
+    return currencies[safeConfig.fiatSymbol];
+  }, [safeConfig?.fiatSymbol]);
 
-  const formattedBalance = useMemo(() => formatCurrency(balances?.total), [balances?.total]);
-  const formattedPending = useMemo(() => formatCurrency(balances?.pending), [balances?.pending]);
+  const formattedBalance = useMemo(
+    () => formatCurrency(balances?.total, currencyInfo),
+    [balances?.total, currencyInfo],
+  );
+  
+  const formattedPending = useMemo(
+    () => formatCurrency(balances?.pending, currencyInfo),
+    [balances?.pending, currencyInfo],
+  );
 
   return (
     <div className="flex flex-col gap-2 p-4">
@@ -36,7 +53,7 @@ export const Balances = () => {
       ) : (
         <Skeleton className="h-10 w-32 rounded-lg" />
       )}
-      {formattedPending && formattedPending !== "€0.00" && (
+      {balances?.pending && balances.pending !== "0" && (
         <div className="text-secondary flex items-center gap-1">
           <Clock className="w-6 h-6" aria-hidden="true" />
           {formattedPending} pending
---
+++ src/constants.tsx
@@ -0,0 +1,31 @@
+export interface CurrencyInfo {
+  tokenSymbol: string;
+  address: string;
+  decimals: number;
+  symbol: string;
+  fiatSymbol: string;
+}
+
+export const currencies: Record<string, CurrencyInfo> = {
+  EUR: {
+    tokenSymbol: "EURe",
+    address: "0xcB444e90D8198415266c6a2724b7900fb12FC56E",
+    decimals: 18,
+    symbol: "€",
+    fiatSymbol: "EUR",
+  },
+  GPB: {
+    tokenSymbol: "GBPe",
+    address: "0x5Cb9073902F2035222B9749F8fB0c9BFe5527108",
+    decimals: 18,
+    symbol: "£",
+    fiatSymbol: "GPB",
+  },
+  USD: {
+    tokenSymbol: "USDCe",
+    address: "0x2a22f9c3b484c3629090FeED35F17Ff8F88f76F0",
+    decimals: 6,
+    symbol: "$",
+    fiatSymbol: "USD",
+  },
+};
