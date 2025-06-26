8f18c83d9b5ddd8a7e819b88af1a6bcf56bfd809
haroon-schmrkow <163864960+haroon-schmrkow@users.noreply.github.com>
2025-06-26 12:22:26 +0200

    Match colors & header to design (#13)

--- src/components/nav/header.tsx
+++ src/components/nav/header.tsx
@@ -10,30 +10,32 @@ export const HeaderNavBar = () => {
   const { effectiveTheme } = useTheme();
   return (
     <header className="w-full border-b hidden lg:block">
-      <div className="h-14 flex items-center px-4">
-        <div className="grid grid-cols-6 w-full items-center">
-          {/* Logo */}
-          <div className="flex items-center gap-2 col-start-2">
-            <a href="/">
-              <img src={effectiveTheme === "light" ? lightLogo : darklogo} alt="Gnosis Pay logo" />
-            </a>
-          </div>
-          {/* Navigation */}
-          <div className="flex items-center gap-8">
-            {routes.map((route) => (
-              <NavLink
-                key={route.path}
-                to={route.path}
-                className={({ isActive }) =>
-                  `flex items-center gap-2 text-base font-medium transition-colors ${
-                    isActive ? "text-link-active font-normal" : "text-muted-foreground"
-                  }`
-                }
-              >
-                <route.icon size={22} />
-                {route.label}
-              </NavLink>
-            ))}
+      <div className="py-4 flex items-center px-4">
+        <div className="flex items-center justify-between w-full">
+          <div className="flex items-center gap-12">
+            {/* Logo */}
+            <div className="flex items-center gap-2">
+              <a href="/">
+                <img src={effectiveTheme === "light" ? lightLogo : darklogo} alt="Gnosis Pay logo" />
+              </a>
+            </div>
+            {/* Navigation */}
+            <div className="flex items-center gap-8">
+              {routes.map((route) => (
+                <NavLink
+                  key={route.path}
+                  to={route.path}
+                  className={({ isActive }) =>
+                    `flex items-center gap-2 text-sm font-semibold transition-colors ${
+                      isActive ? "text-link-active" : "text-link-secondary"
+                    }`
+                  }
+                >
+                  <route.icon size={16} />
+                  {route.label}
+                </NavLink>
+              ))}
+            </div>
           </div>
           {/* Actions */}
           <div className="col-start-4 col-span-2 flex gap-2 items-center justify-end">
