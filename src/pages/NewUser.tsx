6b63e54dcc2cd00aa3e5d09dfa9f454dbe6a1cdf
Thibaut S <33178835+Tbaut@users.noreply.github.com>
2025-06-26 12:09:29 +0100

    wip otp

--- src/pages/NewUser.tsx
+++ src/pages/NewUser.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import { postApiV1AuthSignupOtp } from "@/client";
+import { postApiV1AuthSignup, postApiV1AuthSignupOtp } from "@/client";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
+import { OtpInput } from "@/components/otpInput";
 
 enum RegisterStep {
   Email = "email",
@@ -15,11 +16,14 @@ export const NewUserRoute = () => {
   const [email, setEmail] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
+  const [otp, setOtp] = useState("");
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleSubmitOtpRequest = async (e: React.FormEvent) => {
     e.preventDefault();
     setIsLoading(true);
     setError("");
+    setOtp("");
+
     try {
       const { error, data } = await postApiV1AuthSignupOtp({
         body: { email },
@@ -39,13 +43,37 @@ export const NewUserRoute = () => {
     }
   };
 
+  const handleOtpSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError("");
+    try {
+      const { error, data } = await postApiV1AuthSignup({
+        body: { authEmail: email, otp },
+      });
+
+      console.log("registered");
+      console.log(data);
+      if (error) {
+        const message = "error" in error ? error.error : "message" in error ? error.message : "unkown";
+        setError(`Error while signin up: ${message}`);
+        console.error(error);
+      }
+    } catch (err) {
+      setError("Error while signing up");
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="grid grid-cols-6 gap-4 h-full mt-4">
       <div className="col-span-6 lg:col-start-2 lg:col-span-4 mx-4 lg:mx-0">
         <h2 className="text-xl">Register</h2>
-        <div>Type your email to receive a 1 time code.</div>
         {step === RegisterStep.Email && (
-          <form className="space-y-4" onSubmit={handleSubmit}>
+          <form className="space-y-4 mt-8" onSubmit={handleSubmitOtpRequest}>
+            <Label htmlFor="register-email">Type your email to receive a 1 time code.</Label>
             <div className="mt-4">
               <Input
                 className="lg:w-1/2"
@@ -60,8 +88,30 @@ export const NewUserRoute = () => {
               />
             </div>
             <Button type="submit" loading={isLoading} disabled={isLoading || !email}>
-              Send verification code
+              Get code
             </Button>
+            {error && (
+              <Alert variant="destructive">
+                <AlertTitle>Error</AlertTitle>
+                <AlertDescription>{error}</AlertDescription>
+              </Alert>
+            )}
+          </form>
+        )}
+        {step === RegisterStep.Otp && (
+          <form className="space-y-4 mt-8" onSubmit={handleOtpSubmit}>
+            <Label htmlFor="otp">Enter the 6-digit code sent to your email</Label>
+            <OtpInput value={otp} onChange={setOtp} isLoading={isLoading} disabled={isLoading} />
+            {error && (
+              <Button variant={"link"} onClick={handleSubmitOtpRequest}>
+                Send new code
+              </Button>
+            )}
+            {!error && (
+              <Button type="submit" loading={isLoading} disabled={isLoading || otp.length !== 6}>
+                Verify and Sign Up
+              </Button>
+            )}
             {error && (
               <Alert variant="destructive">
                 <AlertTitle>Error</AlertTitle>
