a60101a1ab54d3b5bf50e880848bfc91d8a91912
Thibaut S <33178835+Tbaut@users.noreply.github.com>
2025-07-07 16:14:12 +0100

    adapt design

--- src/pages/Cards.tsx
+++ src/pages/Cards.tsx
@@ -1,18 +1,26 @@
 import { CardsCarousel } from "@/components/cards-carousel/cards-carousel";
 import { VirtualCardsOrderModal } from "@/components/modals/virtual-cards-order";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+import { useState } from "react";
 
 export const CardsRoute = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="grid grid-cols-6 gap-8 h-full mt-4 md:px-0">
       <div className="col-span-6 md:col-span-4 md:col-start-2 px-4 sm:px-0">
         <div className="flex items-center justify-between">
           <h1 className="text-2xl font-semibold">Cards</h1>
-          <VirtualCardsOrderModal />
+          <Button variant="default" className="mt-6" onClick={() => setOpen(true)}>
+            <PlusIcon className="w-4 h-4" />
+            Add Card
+          </Button>
         </div>
       </div>
       <div className="col-span-6 md:col-span-4 md:col-start-2">
         <CardsCarousel />
       </div>
+      <VirtualCardsOrderModal open={open} onOpenChange={setOpen} />
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import { Cards } from "../components/cards";
 import { Balances } from "@/components/balances";
 import { Transactions } from "@/components/transactions/transactions";
+import { ChevronRight } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
@@ -9,10 +11,21 @@ export const Home = () => {
         <Balances />
         <div className="grid grid-cols-3 gap-4">
           <div className="col-span-3 mx-4 lg:mx-0 lg:col-span-2">
-            <Transactions />
+            <div className="flex items-center justify-between mb-4">
+              <h1 className="font-bold text-secondary">Transactions</h1>
+              <Link to="/transactions" className="flex items-center gap-2">
+                View all <ChevronRight size={16} />
+              </Link>
+            </div>
+            <Transactions history={7} />
           </div>
           <div className="col-span-3 lg:col-span-1 lg:col-start-3">
-            <h1 className="font-bold text-secondary my-4">Cards</h1>
+            <div className="flex items-center justify-between mb-4">
+              <h1 className="font-bold text-secondary">Cards</h1>
+              <Link to="/cards" className="flex items-center gap-2">
+                View details <ChevronRight size={16} />
+              </Link>
+            </div>
             <Cards />
           </div>
         </div>
