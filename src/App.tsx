53d1bdc5e4b54acc0de2ae9aa3ccb6230a3a20d5
Thibaut S <33178835+Tbaut@users.noreply.github.com>
2025-08-06 13:49:13 +0100

    fix lint again

--- src/App.tsx
+++ src/App.tsx
@@ -13,80 +13,80 @@ import { AuthGuard } from "@/components/AuthGuard";
 import { AccountRoute } from "./pages/Account";
 
 export const menuRoutes = [
-	{
-		path: "/",
-		element: <Home />,
-		icon: HomeIcon,
-		label: "Home",
-	},
-	{
-		path: "/cards",
-		element: <CardsRoute />,
-		icon: CreditCard,
-		label: "Cards",
-	},
-	{
-		path: "/transactions",
-		element: <TransactionsRoute />,
-		icon: List,
-		label: "Transactions",
-	},
-	{
-		path: "/account",
-		element: <AccountRoute />,
-		icon: User,
-		label: "Account",
-	},
+  {
+    path: "/",
+    element: <Home />,
+    icon: HomeIcon,
+    label: "Home",
+  },
+  {
+    path: "/cards",
+    element: <CardsRoute />,
+    icon: CreditCard,
+    label: "Cards",
+  },
+  {
+    path: "/transactions",
+    element: <TransactionsRoute />,
+    icon: List,
+    label: "Transactions",
+  },
+  {
+    path: "/account",
+    element: <AccountRoute />,
+    icon: User,
+    label: "Account",
+  },
 ];
 
 const publicRoutes = [
-	{
-		path: "/register",
-		element: <SignUpRoute />,
-	},
-	{
-		path: "/kyc",
-		element: <KycRoute />,
-	},
-	{
-		path: "/safe-deployment",
-		element: <SafeDeploymentRoute />,
-	},
+  {
+    path: "/register",
+    element: <SignUpRoute />,
+  },
+  {
+    path: "/kyc",
+    element: <KycRoute />,
+  },
+  {
+    path: "/safe-deployment",
+    element: <SafeDeploymentRoute />,
+  },
 ];
 
 function ProtectedLayout({ checkForSignup }: { checkForSignup?: boolean }) {
-	return (
-		<AuthGuard checkForSignup={checkForSignup}>
-			<Outlet />
-		</AuthGuard>
-	);
+  return (
+    <AuthGuard checkForSignup={checkForSignup}>
+      <Outlet />
+    </AuthGuard>
+  );
 }
 
 function App() {
-	console.log("+++ env variables +++");
-	console.log(import.meta.env.VITE_PSE_RELAY_SERVER_URL);
-	console.log(import.meta.env.VITE_IFRAME_HOST);
-	console.log(import.meta.env.VITE_GNOSIS_PAY_API_BASE_URL);
-	console.log(import.meta.env.VITE_PSE_APP_ID);
+  console.log("+++ env variables +++");
+  console.log(import.meta.env.VITE_PSE_RELAY_SERVER_URL);
+  console.log(import.meta.env.VITE_IFRAME_HOST);
+  console.log(import.meta.env.VITE_GNOSIS_PAY_API_BASE_URL);
+  console.log(import.meta.env.VITE_PSE_APP_ID);
 
-	return (
-		<div className="flex min-h-screen flex-col">
-			<HeaderNavBar />
-			<Routes>
-				<Route element={<ProtectedLayout checkForSignup={false} />}>
-					{publicRoutes.map((route) => (
-						<Route key={route.path} path={route.path} element={route.element} />
-					))}
-				</Route>
-				<Route element={<ProtectedLayout checkForSignup={true} />}>
-					{menuRoutes.map((route) => (
-						<Route key={route.path} path={route.path} element={route.element} />
-					))}
-				</Route>
-			</Routes>
-			<FooterNavBar />
-		</div>
-	);
+  return (
+    <div className="flex min-h-screen flex-col">
+      <HeaderNavBar />
+      <Routes>
+        <Route element={<ProtectedLayout checkForSignup={false} />}>
+          {publicRoutes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+        </Route>
+        <Route element={<ProtectedLayout checkForSignup={true} />}>
+          {menuRoutes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+        </Route>
+      </Routes>
+      <FooterNavBar />
+    </div>
+  );
 }
 
 export default App;
