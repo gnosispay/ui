b106f6ac758b76095936367fffd823f4bc41e7dc
Thibaut S <33178835+Tbaut@users.noreply.github.com>
2025-08-18 11:17:23 +0100

    8000 for the limit

--- src/constants.ts
+++ src/constants.ts
@@ -57,4 +57,4 @@ export const userTermsTitle: Record<UserTermsTypeFromApi, string> = {
 
 export const GNOSIS_PAY_SETTLEMENT_ADDRESS = "0x4822521E6135CD2599199c83Ea35179229A172EE";
 
-export const MAX_DAILY_LIMIT = 5000;
+export const MAX_DAILY_LIMIT = 8000;
