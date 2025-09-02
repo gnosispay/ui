66187c325dbe0b6c8143a5b181a676779fcc2be0
Thibaut S <33178835+Tbaut@users.noreply.github.com>
2025-09-02 17:02:46 +0100

    walletconnect

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,30 +1,28 @@
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { http, createConfig } from "wagmi";
 import { gnosis } from "wagmi/chains";
-import { http } from "wagmi";
+import { coinbaseWallet, injected, safe, walletConnect } from "wagmi/connectors";
 
-export const config = getDefaultConfig({
-  appName: "Gnosis Pay",
-  appDescription: "Gnosis Pay - Your gateway to DeFi payments",
-  appUrl: "https://v2.gnosispay.com",
-  appIcon: "https://framerusercontent.com/images/gF4AHnqudSqMqpTf5kFE93eQuHs.png",
-  projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID || "2f5a2b1c8e9d4f3a6b7c8d9e0f1a2b3c", // Get your own from https://cloud.reown.com/
+export const config = createConfig({
   chains: [gnosis],
-  // connectors: [
-  //   safe({
-  //     allowedDomains: [/gnosis-safe.io$/, /app.safe.global$/],
-  //     debug: false,
-  //     shimDisconnect: false,
-  //   }),
-  //   injected(),
-  //   coinbaseWallet(),
-  // ],
+  connectors: [
+    safe({
+      allowedDomains: [/gnosis-safe.io$/, /app.safe.global$/],
+      debug: false,
+      shimDisconnect: false,
+    }),
+    injected(),
+    coinbaseWallet(),
+    walletConnect({
+      projectId: "97f66822692378042f2b9a3c6d370c2b",
+    }),
+  ],
   transports: {
     [gnosis.id]: http(),
   },
 });
 
-declare module "wagmi" {
-  interface Register {
-    config: typeof config;
-  }
-}
+// declare module "wagmi" {
+//   interface Register {
+//     config: typeof config;
+//   }
+// }
