3cec8e8b43b83dc0e597e33bbdbcec5844fcc237
Thibaut S <33178835+Tbaut@users.noreply.github.com>
2025-10-15 10:52:23 +0100

    while I'm at it.. remove lonely lib

--- src/components/ui/icon-button.tsx
+++ src/components/ui/icon-button.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
-import { cn } from "@/lib/utils"
+import { cn } from "@/utils/cn"
 
 const iconButtonVariants = cva(
   "cursor-pointer inline-flex items-center justify-center transition-all disabled:pointer-events-none disabled:opacity-50 outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
