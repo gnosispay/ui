c36eb32be4f03370c2cb334bd85b8a231a36d652
Kemal Mustafic <kemalmustafic@users.noreply.github.com>
2026-06-06 21:04:56 +0200

    fix: lint

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -37,9 +37,7 @@ export const Home = () => {
               <Button onClick={() => setSendFundsModalOpen(true)} data-testid="send-funds-button">
                 Send funds
               </Button>
-              <Button onClick={() => setAddFundsModalOpen(true)}>
-                Add funds
-              </Button>
+              <Button onClick={() => setAddFundsModalOpen(true)}>Add funds</Button>
             </div>
           </div>
 
